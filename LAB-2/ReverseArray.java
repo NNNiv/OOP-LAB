5d5982de48b4c90bd446a43ac8251891d2c10524
nnniv <nivroh2016@gmail.com>
2023-08-22 21:16:28 +0530

    idek

--- LAB-2/ReverseArray.java
+++ LAB-2/ReverseArray.java
@@ -5,7 +5,7 @@ public class ReverseArray {
 	public static void main(String[] args) {
 		int[] arr = new int[10];
 		int n;
-		int start, end;
+		int temp;
 
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the number of elements (max 10): ");
@@ -20,10 +20,9 @@ public class ReverseArray {
 
 		for (int i = 0; i < n / 2; i++) {
 			
-			end = arr[n - i - 1]; 
-			start = arr[i];
-			arr[n - i - 1] = start;
-			arr[i] = end;
+			temp = arr[n - i - 1]; 
+			arr[n - i - 1] = arr[i];
+			arr[i] = temp;
 		}
 		System.out.println("The reversed array is: ");
 		for (int i = 0; i < n; i++) {
