411ac09f95207dd4ea95046f9f0b8e03a05e2e18
nnniv <nivroh2016@gmail.com>
2023-09-05 23:02:53 +0530

    fix indentation

--- LAB-4/BankAccount.java
+++ LAB-4/BankAccount.java
@@ -1,3 +1,4 @@
+
 // DO NOT COPY
 // ITS BROKEN
 import java.util.*;
@@ -8,14 +9,15 @@ public class BankAccount {
 	String accountType;
 	double balance;
 	static double rate;
-	
+
 	// Default Constructor
 	public BankAccount() {
-    assign();
+		assign();
 	}
-  void assign() {
+
+	void assign() {
 		System.out.println("Created new bank account");
-		
+
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter depositor's name: ");
 		depositorName = sc.next();
@@ -24,11 +26,11 @@ public class BankAccount {
 		System.out.println("Enter account type: ");
 		accountType = sc.next();
 		System.out.println("Enter account balance: ");
-    balance = sc.nextDouble();
+		balance = sc.nextDouble();
 		System.out.println("Enter rate: ");
-    rate = sc.nextDouble();
-  }
-	
+		rate = sc.nextDouble();
+	}
+
 	// Parameterized Constructor
 	public BankAccount(String name, int accNum, String accType, double accBalance, double accRate) {
 		depositorName = name;
@@ -37,11 +39,12 @@ public class BankAccount {
 		balance = accBalance;
 		rate = accRate;
 	}
+
 	public void deposit(int amount) {
 		balance += amount;
 		System.out.println("Deposited Rs. " + amount);
 	}
-	
+
 	public void withdraw(int amount) {
 		if (balance - amount <= 0) {
 			System.out.println("Insufficient balance");
@@ -51,87 +54,89 @@ public class BankAccount {
 			System.out.println("Balance: Rs. " + balance);
 		}
 	}
-	
+
 	public void displayDetails() {
 		System.out.println("Depositor's name: " + depositorName);
 		System.out.println("Account Number: " + accountNumber);
-		System.out.println("Account Type: "+ accountType);
+		System.out.println("Account Type: " + accountType);
 		System.out.println("Account Balance: " + balance);
 	}
-	
+
 	static void displayRate() {
 		System.out.println("Rate of interest: " + rate);
 	}
-	
+
 	public static void main(String[] args) {
 		System.out.println("Demonstrating default constructor");
 		BankAccount acc1 = new BankAccount();
 		int choice;
 		int amount;
 		Scanner sc = new Scanner(System.in);
-		
+
 		while (true) {
-			System.out.println("Would you like to\n1. Deposit\n2. Withdraw\n3. Display Details\n4. Diplay Rate\n5. Exit");
+			System.out
+					.println("Would you like to\n1. Deposit\n2. Withdraw\n3. Display Details\n4. Diplay Rate\n5. Exit");
 			System.out.println("Enter your choice: ");
 			choice = sc.nextInt();
-			
+
 			switch (choice) {
-			
-			case 1:
-				System.out.println("Enter amount to deposit: ");
-				amount = sc.nextInt();
-				acc1.deposit(amount);
-        continue;
-			
-			case 2:
-				System.out.println("Enter amount to withdraw: ");
-				amount = sc.nextInt();
-				acc1.withdraw(amount);
-        continue;
 
-			case 3:
-				acc1.displayDetails();
-        continue;
+				case 1:
+					System.out.println("Enter amount to deposit: ");
+					amount = sc.nextInt();
+					acc1.deposit(amount);
+					continue;
 
-			case 4:
-				displayRate();
-        continue;
-        
-      case 5:
-        break;
+				case 2:
+					System.out.println("Enter amount to withdraw: ");
+					amount = sc.nextInt();
+					acc1.withdraw(amount);
+					continue;
+
+				case 3:
+					acc1.displayDetails();
+					continue;
+
+				case 4:
+					displayRate();
+					continue;
+
+				case 5:
+					break;
 			}
-      break;
+			break;
 		}
-		
+
 		System.out.println();
 		System.out.println("Demonstrating parameterized constructors");
 		BankAccount acc2 = new BankAccount("Joe", 1221, "Savings", 10000, 0.5);
 		while (true) {
-			System.out.println("Would you like to\n1. Deposit\n2. Withdraw\n3. Display Details\n4. Diplay Rate\n5. Exit");
+			System.out
+					.println("Would you like to\n1. Deposit\n2. Withdraw\n3. Display Details\n4. Diplay Rate\n5. Exit");
 			System.out.println("Enter your choice: ");
 			choice = sc.nextInt();
-			
+
 			switch (choice) {
-			
-			case 1:
-				System.out.println("Enter amount to deposit: ");
-				amount = sc.nextInt();
-				acc2.deposit(amount);
-        continue;
-			
-			case 2:
-				System.out.println("Enter amount to withdraw: ");
-				amount = sc.nextInt();
-				acc2.withdraw(amount);
-        continue;
-			case 3:
-				acc2.displayDetails();
-        continue;
-			case 4:
-				displayRate();
-        continue;
-			case 5:
-				break;
+
+				case 1:
+					System.out.println("Enter amount to deposit: ");
+					amount = sc.nextInt();
+					acc2.deposit(amount);
+					continue;
+
+				case 2:
+					System.out.println("Enter amount to withdraw: ");
+					amount = sc.nextInt();
+					acc2.withdraw(amount);
+					continue;
+				case 3:
+					acc2.displayDetails();
+					continue;
+				case 4:
+					displayRate();
+					continue;
+				case 5:
+					break;
 			}
 			break;
 		}
--- LAB-4/Employee.java
+++ LAB-4/Employee.java
@@ -1,16 +1,20 @@
 import java.util.Scanner;
 
 public class Employee {
-	String eName;
-	String eID;
-	int basic;
-	double da;
-	double grossSalary;
-	double netSalary;
-	
-	// Default Constructor
-	public Employee() {
-		System.out.println("Created new employee record");
+    String eName;
+    String eID;
+    int basic;
+    double da;
+    double grossSalary;
+    double netSalary;
+
+    // Default Constructor
+    public Employee() {
+        assign();
+    }
+
+    void assign() {
+        System.out.println("Created new employee record");
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Enter employee name: ");
@@ -21,20 +25,20 @@ public class Employee {
 
         System.out.println("Enter basic pay: ");
         basic = sc.nextInt();
-        
+
         computeNetSal();
         sc.close();
     }
-	
-	// Parameterized Constructor
-	public Employee(String name, String id, int basicPay) {
-		eName  = name;
-		eID = id;
-		basic = basicPay;
-		computeNetSal();
-	}
-	
-	public void display() {
+
+    // Parameterized Constructor
+    public Employee(String name, String id, int basicPay) {
+        eName = name;
+        eID = id;
+        basic = basicPay;
+        computeNetSal();
+    }
+
+    public void display() {
         System.out.println("Employee name: " + eName);
         System.out.println("Employee ID: " + eID);
         System.out.println("Employee basic pay: Rs. " + basic);
@@ -48,14 +52,14 @@ public class Employee {
         grossSalary = basic + da;
         netSalary = 0.7 * grossSalary;
     }
-    
+
     public static void main(String args[]) {
-    	System.out.println("Demonstrating default constructors");
-    	Employee emp1 = new Employee();
-    	emp1.display();
-    	System.out.println();
-    	System.out.println("Demonstrating parameterized constructors");
-    	Employee emp2 = new Employee("Nived", "1221", 5000);
-    	emp2.display();
+        System.out.println("Demonstrating default constructors");
+        Employee emp1 = new Employee();
+        emp1.display();
+        System.out.println();
+        System.out.println("Demonstrating parameterized constructors");
+        Employee emp2 = new Employee("Nived", "1221", 5000);
+        emp2.display();
     }
 }
