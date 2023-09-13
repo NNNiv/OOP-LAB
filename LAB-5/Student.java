2250cfd3576f8c4a36d848e7b75e327e0c6ffa07
nnniv <nivroh2016@gmail.com>
2023-09-13 09:03:12 +0530

    idk

--- LAB-5/Student.java
+++ LAB-5/Student.java
@@ -1,3 +1,5 @@
+package lab_5;
+
 import java.util.Scanner;
 
 public class Student {
@@ -9,10 +11,15 @@ public class Student {
     // Default Constructor
     public Student() {
         System.out.println("Created new student record");
-        Scanner sc = new Scanner(System.in);
+        assign();
+        compute();
+        
+    }
+    void assign() {
+    	Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter the name: ");
-        sName = sc.next();
+        sName = sc.nextLine();
 
         int n;
         System.out.print("Enter the number of subjects: ");
@@ -22,18 +29,66 @@ public class Student {
 
         for (int i = 0; i < n; i++)
             marksArray[i] = sc.nextInt();
-
-        compute();
-        sc.close();
     }
-
     // Parameterized Constructor
     public Student(String name, int marks[]) {
         sName = name;
         marksArray = marks;
         compute();
     }
-
+    
+    public String extractInitials() {
+    	String initial = new String();
+    	String[] names = sName.split(" ");
+    	for (String name: names) {
+    		initial += name.substring(0, 1).toUpperCase();
+    	}
+    	
+    	return initial;
+    	
+    }
+ 
+    public String removeWhitespace() {
+    	String noSpace = new String();
+    	String[] names = sName.split(" ");
+    	for (String name: names) {
+    		noSpace += name;
+    	}
+    	return noSpace;
+    	
+    }
+    public static String[] substring(Student[] students, String subStr) {
+    	String names[] = new String[30];
+    	int i = 0;
+    	for (Student student: students) {
+    		if (student.sName.toLowerCase().contains(subStr.toLowerCase())) {
+    			names[i] = student.sName;
+    			i++;
+    		}
+    	}
+    			
+    	return names;
+    	
+    }
+    public static String[] sort(Student[] students) {
+    	String names[] = new String[30];
+    	int i = 0;
+    	for (Student student: students) {
+    		names[i] = student.sName;
+    		i++;
+    	}
+    	
+    	for (i = 0; i < names.length; i++)
+    		for (int j = i + 1; j < names.length; j++)
+    			if (names[i] == null && names[j] != null)
+    				if (names[i].compareTo(names[j]) > 0) {
+    					String temp = names[i];
+    					names[i] = names[j];
+    					names[j] = temp;
+     			}
+    	return names;
+    	
+    }
     public void compute() {
         for (int i = 0; i < marksArray.length; i++)
             total += marksArray[i];
@@ -51,20 +106,41 @@ public class Student {
         System.out.println("Average marks: " + avg);
     }
 
-    // public void sort() {
-    // Student[] s1 = new Student[10];
-    // }
 
     public static void main(String[] args) {
-        System.out.println("Demonstrating default constructor");
-        Student stu1 = new Student();
-        stu1.display();
+        Scanner sc = new Scanner(System.in);
+    	int n;
+        System.out.print("Enter the number of students: ");
+        n = sc.nextInt();
+        Student students[] = new Student[n];
+        for (int i = 0; i < n; i++) {
+        	students[i] = new Student();
+        	System.out.println("The student's initials are: " + students[i].extractInitials());
+        	System.out.println("The student's name without whitespaces: "+ students[i].removeWhitespace());
+        }
+        
+        String subStr = new String();
+        System.out.print("Enter substring: ");
+        subStr = sc.next();
+        String[] subNames = substring(students, subStr);
+        System.out.println("\nNames containing the substring: ");
+        for (String name: subNames) {
+        	if (name != null)
+        		System.out.println(name);
+        }
+        
+        String[] sortedNames = sort(students);
+        System.out.println("\nThe sorted list of names: ");
+        for (String name: sortedNames) {
+        	if (name != null)
+        		System.out.println(name);
+        }
 
-        System.out.println();
-
-        System.out.println("Demonstarting parameterized constructor");
-        int[] marks = { 10, 11, 12, 12, 14 };
-        Student stu2 = new Student("Nived", marks);
-        stu2.display();
+    	/*
+    	int[] marks = { 10, 11 };
+        Student obj = new Student("Nived Mohan", marks);
+        System.out.println(obj.extractInitials());
+        System.out.println(obj.removeWhitespace());
+        */
     }
 }
