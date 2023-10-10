764d8706fdd08713ac24610c12fbb82bf23d3904
nnniv <nivroh2016@gmail.com>
2023-10-10 18:58:52 +0530

    I wrote this two weeks ago so don't @ me if something is wrong :)

---
+++ LAB-6/Employee/Main.java
@@ -0,0 +1,128 @@
+package lab_6_employee;
+
+import java.util.Scanner;
+
+class Employee {
+    String eName;
+    String eID;
+    int basic;
+    double da;
+    double grossSalary;
+    double netSalary;
+
+    public Employee(){
+    }
+    
+    void read() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter employee name: ");
+        eName = sc.nextLine();
+
+        System.out.println("Enter employee ID: ");
+        eID = sc.next();
+
+        System.out.println("Enter basic pay: ");
+        basic = sc.nextInt();
+    }
+
+    void displayEmployeeDetails() {
+        System.out.println("Employee name: " + eName);
+        System.out.println("Employee ID: " + eID);
+        System.out.println("Employee basic pay: " + basic);
+        System.out.println("Employee DA: " + da);
+        System.out.println("Employee gross salary: " + grossSalary);
+        System.out.println("Employee net salary: " + netSalary);
+
+    }
+
+    void calculateSalary() {
+        da = 0.52 * basic;
+        grossSalary = basic + da;
+        netSalary = 0.7 * grossSalary;
+    }
+}
+
+class FullTimeEmp extends Employee {
+	
+	
+	int hoursWorked;
+	final static double hourlyRate = 420;
+	int bonus;
+	int deductions;
+	
+	public FullTimeEmp() {
+		System.out.println("Enter full time employee details: ");
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter employee name: ");
+		eName = sc.nextLine();
+		System.out.println("Enter employee ID: ");
+		eID = sc.next();
+		System.out.println("Enter hours worked: ");
+		hoursWorked = sc.nextInt();
+		System.out.println("Enter bonus: ");
+		bonus = sc.nextInt();
+		System.out.println("Enter deductions: ");
+		deductions = sc.nextInt();
+		
+		calculateSalary();
+	}
+	
+	@Override void calculateSalary() {
+		netSalary = (hoursWorked * hourlyRate) - bonus + deductions;
+	}
+	
+	@Override void displayEmployeeDetails() {
+		System.out.println("Employee name: "+ eName);
+		System.out.println("Employee ID: " + eID);
+		System.out.println("Employee Salary: " + netSalary);
+	}
+	
+}
+
+class PartTimeEmp extends Employee {
+	int hoursWorked;
+	final static double hourlyRate = 420;
+	
+	public PartTimeEmp() {
+		System.out.println("Enter part time employee details: ");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter employee name: ");
+		eName = sc.nextLine();
+		System.out.println("Enter employee ID: ");
+		eID = sc.next();
+		System.out.println("Enter hours worked: ");
+		hoursWorked = sc.nextInt();
+		
+		calculateSalary();
+	}
+	
+	@Override void calculateSalary() {
+		netSalary = hoursWorked * hourlyRate;
+	}
+	
+	@Override void displayEmployeeDetails() {
+		System.out.println("Employee name: "+ eName);
+		System.out.println("Employee ID: " + eID);
+		System.out.println("Employee Salary: " + netSalary);
+	}
+}
+
+public class Main {
+	public static void main(String args[]) {
+		Employee emp = new Employee();
+		emp.read();
+		emp.calculateSalary();
+		FullTimeEmp empFull = new FullTimeEmp();
+		PartTimeEmp empPart = new PartTimeEmp();
+		
+		
+		emp.displayEmployeeDetails();
+		System.out.println();
+		empFull.displayEmployeeDetails();
+		System.out.println();
+		empPart.displayEmployeeDetails();
+	}
+
+}
---
+++ LAB-6/Student/Main.java
@@ -0,0 +1,107 @@
+package lab_6_student;
+
+import java.util.Scanner;
+
+class Student {
+    String sName;
+    int marksArray[];
+    double total = 0;
+    double avg;
+
+    public Student() {
+        System.out.println("Created new student record");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the name: ");
+        sName = sc.next();
+
+        int n;
+        System.out.print("Enter the number of subjects: ");
+        n = sc.nextInt();
+        marksArray = new int[n];
+        System.out.print("Enter the marks of " + n + " subjects: ");
+
+        for (int i = 0; i < n; i++)
+            marksArray[i] = sc.nextInt();
+    }
+    public void compute() {
+        for (int i = 0; i < marksArray.length; i++)
+            total += marksArray[i];
+
+        avg = total / marksArray.length;
+    }
+
+    public void display() {
+        System.out.println("\nStudent name: " + sName);
+        System.out.println("The Student's marks are: ");
+        for (int i = 0; i < marksArray.length; i++)
+            System.out.println(marksArray[i]);
+        System.out.println("Marks total: " + total);
+        System.out.println("Average marks: " + avg);
+    }
+}
+
+class ScienceStudent extends Student {
+	private int practicalMarks;
+	
+	public ScienceStudent() {
+		super();
+		getPracticalMarks();
+		compute();
+	}
+	
+	Scanner sc = new Scanner(System.in);
+	public void getPracticalMarks() {
+		System.out.println("Enter practical marks: ");
+		practicalMarks = sc.nextInt();
+	}
+	
+	@Override public void compute() {
+		for (int i = 0; i < marksArray.length; i++)
+			total += marksArray[i];
+		total += practicalMarks;
+		avg = total / (marksArray.length + 1);
+	};
+
+	public void displayPracticalMarks() {
+		System.out.println("Practical Marks: " + practicalMarks);
+		System.out.println();
+	}
+}
+
+class ArtsStudent extends Student {
+	private String electiveSubject;
+	Scanner sc = new Scanner(System.in);
+	public ArtsStudent() {
+		super();
+		getElectiveSubject();
+		compute();
+	}
+	
+	public void getElectiveSubject() {
+		System.out.println("Enter elective subject: ");
+		electiveSubject = sc.next();
+	}
+	
+	public void displayElectiveSubject() {
+		System.out.println("Elective Subject: " + electiveSubject);
+		System.out.println();
+	}
+}
+
+public class Main {
+	public static void main(String[] args) {
+		Student stud = new Student();
+		ScienceStudent sciStud = new ScienceStudent();
+		ArtsStudent artsStud = new ArtsStudent();
+		stud.compute();
+		stud.display();
+		
+		sciStud.display();
+		sciStud.displayPracticalMarks();
+		
+		artsStud.compute();
+		artsStud.display();
+		artsStud.displayElectiveSubject();
+	}
+}
